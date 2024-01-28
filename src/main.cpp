aeb5da9191571d7db9dd79b5be5c7220c64af9e7
Rene <rzeldent@users.noreply.github.com>
2024-01-28 18:43:53 +0100

    Develop (#17)
    * Added 2432S022N
    * Added 8048S070R
    * Smaller Lena 80x80
    * Removed v11 boards
    * Default no rotation
    * Updated build for node 20
    * No default_env

--- src/main.cpp
+++ src/main.cpp
@@ -45,7 +45,7 @@ void setup()
     smartdisplay_init();
 
     auto disp = lv_disp_get_default();
-    lv_disp_set_rotation(disp, LV_DISP_ROT_90);
+    // lv_disp_set_rotation(disp, LV_DISP_ROT_90);
     // lv_disp_set_rotation(disp, LV_DISP_ROT_180);
     // lv_disp_set_rotation(disp, LV_DISP_ROT_270);
 
