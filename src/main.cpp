ea8e155b02c369462858394d8993d251533a95ea
Rene Zeldenthuis <rzeldent@outlook.com>
2024-03-07 22:55:56 +0100

    Removed audio

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,6 @@
 
 #include <esp32_smartdisplay.h>
 #include <ui/ui.h>
-#include <Audio.h>
-
-#define WIFI_SSID "<your ssid>"
-#define WIFI_PASSWORD "<your ap password>"
-#define RADIO_URL "http://www.wdr.de/wdrlive/media/einslive.m3u"
-
-Audio *audio;
 
 void OnButtonClicked(lv_event_t *e)
 {
@@ -30,20 +23,6 @@ void setup()
     log_i("Free PSRAM: %d bytes", ESP.getPsramSize());
     log_i("SDK version: %s", ESP.getSdkVersion());
 
-#ifdef BOARD_HAS_SPEAK
-    // Connect to WiFi
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    if (WiFi.waitForConnectResult() == WL_CONNECTED)
-    {
-        audio = new Audio(true, I2S_DAC_CHANNEL_LEFT_EN);
-        audio->forceMono(true);
-        audio->setVolume(10);
-
-        while (!audio->connecttohost(RADIO_URL))
-            delay(500);
-    }
-#endif
-
     smartdisplay_init();
 
     auto disp = lv_disp_get_default();
@@ -58,11 +37,6 @@ ulong next_millis;
 
 void loop()
 {
-#ifdef BOARD_HAS_SPEAK
-    if (audio)
-        audio->loop();
-#endif
-
     auto const now = millis();
     if (now > next_millis)
     {
