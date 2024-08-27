e864a6c642e851d496cd6a22a886908451dfe469
Rene <rzeldent@users.noreply.github.com>
2024-08-27 22:13:56 +0200

    Lvgl9 (#36)
    * Initial commit vor LVGL v9.1
    
    * Changed memory LV_USE_STDLIB to use LV_STDLIB_CLIB
    
    * Updated verison history
    
    * Added lv_tick_inc
    
    * Minor changes
    
    * Enable all boards
    
    * Updated lv_conf.h
    
    * UPdated demo with gradient
    
    * Updated UI SquareLine
    
    * Update to Squareline 1.4.2
    
    * Added new boards
    Use #develop

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,16 @@
 
 void OnAddOneClicked(lv_event_t *e)
 {
-    static uint8_t cnt = 0;
+    static uint32_t cnt = 0;
     cnt++;
-    lv_label_set_text_fmt(ui_lblCountValue, "%d", cnt);
+    lv_label_set_text_fmt(ui_lblCountValue, "%u", cnt);
 }
 
 void OnRotateClicked(lv_event_t *e)
 {
     auto disp = lv_disp_get_default();
-    auto rotation = (lv_disp_rot_t)((lv_disp_get_rotation(disp) + 1) % (LV_DISP_ROT_270 + 1));
-    lv_disp_set_rotation(disp, rotation);
+    auto rotation = (lv_display_rotation_t)((lv_disp_get_rotation(disp) + 1) % (LV_DISPLAY_ROTATION_270 + 1));
+    lv_display_set_rotation(disp, rotation);
 }
 
 void setup()
@@ -40,13 +40,17 @@ void setup()
     ui_init();
 
     // To use third party libraries, enable the define in lv_conf.h: #define LV_USE_QRCODE 1
-    auto ui_qrcode = lv_qrcode_create(ui_scrMain, 100, lv_color_black(), lv_color_white());
+    auto ui_qrcode = lv_qrcode_create(ui_scrMain);
+    lv_qrcode_set_size(ui_qrcode, 100);
+    lv_qrcode_set_dark_color(ui_qrcode, lv_color_black());
+    lv_qrcode_set_light_color(ui_qrcode, lv_color_white());
     const char *qr_data = "https://github.com/rzeldent/esp32-smartdisplay";
     lv_qrcode_update(ui_qrcode, qr_data, strlen(qr_data));
     lv_obj_center(ui_qrcode);
 }
 
 ulong next_millis;
+auto lv_last_tick = millis();
 
 void loop()
 {
@@ -71,5 +75,9 @@ void loop()
 #endif
     }
 
+    // Update the ticker
+    lv_tick_inc(now - lv_last_tick);
+    lv_last_tick = now;
+    // Update the UI
     lv_timer_handler();
 }
